f097ff290007b651537ded2bf734e75a86922cbd
FabianSchubert <fj.schubert@freenet.de>
2023-06-15 15:12:26 +0000

    change input and output dimension on randman task

--- mc/experiments/randman/run.py
+++ mc/experiments/randman/run.py
@@ -13,36 +13,33 @@ from itertools import product
 
 from tqdm import tqdm
 
-D_EMBEDDING = 20
+import time
+
 D_MF = 1
-N_CLASSES = 10
 N_CUTOFF = 1000
 ALPHA = 1.0
 
 SEED_MF = 42
 
-N_HIDDEN = np.linspace(100, 500, 2).astype("int").tolist()
+N_SWEEP_NET_SIZE = 2
+N_HIDDEN = np.linspace(10, 100, N_SWEEP_NET_SIZE).astype("int").tolist()
+N_INPUT = [n + 20 - N_HIDDEN[0] for n in N_HIDDEN]
+N_OUTPUT = [n + 10 - N_HIDDEN[0] for n in N_HIDDEN]
 
-SPIKE_THRESHOLDS = np.exp(np.linspace(np.log(1e-5), np.log(1e-1), 3))
+NET_SIZE_ZIP = list(zip(N_INPUT, N_HIDDEN, N_OUTPUT))
+
+N_SWEEP_THRESHOLDS = 2
+SPIKE_THRESHOLDS = np.exp(np.linspace(np.log(1e-5), np.log(1e-1), N_SWEEP_THRESHOLDS))
                    #np.append(10.**np.arange(-10, -2),
                    #          np.exp(np.linspace(np.log(1e-2), np.log(5e-1), 15)))
 
 N_SAMPLES_TRAIN = 1500
 N_SAMPLES_TEST = 1000
 
-N_EPOCHS = 15
+N_EPOCHS = 1500
 T_SHOW_PATTERN = 150.
 
 AX_REC_MOD_PARAMS = np.arange(N_EPOCHS) * T_SHOW_PATTERN
-
-input_train, output_train = randman_dataset_array(D_EMBEDDING, N_CLASSES,
-                                                  N_SAMPLES_TRAIN, D_MF, N_CUTOFF, ALPHA,
-                                                  seed_mf=SEED_MF)
-input_test, output_test = randman_dataset_array(D_EMBEDDING, N_CLASSES, N_SAMPLES_TEST,
-                                                D_MF, N_CUTOFF, ALPHA, seed_mf=SEED_MF)
-
-data = dict(zip(["input_train", "output_train", "input_test", "output_test"],
-                [input_train, output_train, input_test, output_test]))
 
 BASE_FOLD = os.path.dirname(__file__)
 
@@ -57,9 +54,9 @@ DEFAULT_ADAM_PARAMS = {
 
 
 params_base = {
-    "n_in": D_EMBEDDING,
+    "n_in": None,
     "n_hidden": None,
-    "n_out": N_CLASSES,
+    "n_out": None,
     "dt": 1.0,
     "n_runs": 1,
     "n_epochs": N_EPOCHS,
@@ -114,8 +111,8 @@ params_backprop = {"force_self_pred_state": True,
                    "force_fb_align": True}
 
 method_params = {
-    "Feedback Align": params_fb_align#,
-    #"Backprop": params_backprop
+    "Feedback Align": params_fb_align,
+    "Backprop": params_backprop
 }
 
 models = {
@@ -125,81 +122,114 @@ models = {
 
 df_learn = pd.DataFrame(columns=["Epoch", "Sim ID", "Accuracy",
                                  "Loss", "Model", "Method",
-                                 "Spike Threshold", "N Hidden"])
+                                 "Spike Threshold",
+                                 "N Input", "N Hidden", "N Output"])
 df_runtime = pd.DataFrame(columns=["Runtime", "Sim ID", "Model", "Method",
-                                   "Spike Threshold", "N Hidden"])
+                                   "Spike Threshold",
+                                   "N Input", "N Hidden", "N Output"])
 
-sweep_params = product(models.items(), method_params.items(), N_HIDDEN, SPIKE_THRESHOLDS)
+sweep_params = product(models.items(), method_params.items(), NET_SIZE_ZIP, SPIKE_THRESHOLDS)
 
-for (model_name, model), (method_name, method_param), n_hidden, spike_th in tqdm(sweep_params):
+n_sweep = len(models) * len(method_params) * len(N_HIDDEN) * len(SPIKE_THRESHOLDS)
 
-    _tmp_params = params_base | method_param | {"n_hidden": [n_hidden]}
+with open(os.path.join(BASE_FOLD, "runtime_est.log"), "a") as file_log:
 
-    model.neurons.pyr.mod_dat["param_space"]["th"] = spike_th
-    model.neurons.output.mod_dat["param_space"]["th"] = spike_th
-    model.neurons.int.mod_dat["param_space"]["th"] = spike_th
-    model.neurons.input.mod_dat["param_space"]["th"] = spike_th
+    t0 = time.time()
 
-    epoch_ax, acc, loss, rec_neur, rec_syn, run_time = train_and_test_network(_tmp_params,
-                                                           model, data)
+    for k_sweep, ((model_name, model), (method_name, method_param), net_size_zip, spike_th) in enumerate(sweep_params):
 
-    w_op = rec_syn["syn_hidden0_pyr_pop_to_output_output_pop_g"]
-    w_po = rec_syn["syn_output_output_pop_to_hidden0_pyr_pop_g"]
-    w_po_t = w_po.swapaxes(1,2)
-    w_ip = rec_syn["syn_hidden0_pyr_pop_to_int_pop_g"]
-    w_pi = rec_syn["syn_hidden0_int_pop_to_pyr_pop_g"]
+        input_train, output_train = randman_dataset_array(net_size_zip[0], net_size_zip[2],
+                                                          N_SAMPLES_TRAIN, D_MF, N_CUTOFF, ALPHA,
+                                                          seed_mf=SEED_MF)
+        input_test, output_test = randman_dataset_array(net_size_zip[0], net_size_zip[2],
+                                                        N_SAMPLES_TEST,
+                                                        D_MF, N_CUTOFF, ALPHA,
+                                                        seed_mf=SEED_MF)
 
-    w_fb_aln = (w_op * w_po_t).sum(axis=(1,2)) / np.sqrt((w_op**2.).sum(axis=(1,2)) * (w_po_t**2.).sum(axis=(1,2)))
+        data = dict(zip(["input_train", "output_train", "input_test", "output_test"],
+                    [input_train, output_train, input_test, output_test]))
 
-    w_self_pred_fwd_aln = (w_op * w_ip).sum(axis=(1,2)) / np.sqrt((w_op**2.).sum(axis=(1,2)) * (w_ip**2.).sum(axis=(1,2)))
+        _tmp_params = params_base | method_param | {"n_in": net_size_zip[0],
+                                                    "n_hidden": [net_size_zip[1]],
+                                                    "n_out": net_size_zip[2]}
 
-    w_self_pred_bw_aln = (w_po * w_pi).sum(axis=(1,2)) / np.sqrt((w_po**2.).sum(axis=(1,2)) * (w_pi**2.).sum(axis=(1,2)))
+        model.neurons.pyr.mod_dat["param_space"]["th"] = spike_th
+        model.neurons.output.mod_dat["param_space"]["th"] = spike_th
+        model.neurons.int.mod_dat["param_space"]["th"] = spike_th
+        model.neurons.input.mod_dat["param_space"]["th"] = spike_th
 
-    b_i = rec_neur["neur_hidden0_int_pop_b"][:,0] # only use the first batch (they are all the same value across batches anyway)
-    b_o = rec_neur["neur_output_output_pop_b"][:,0] # ""
+        epoch_ax, acc, loss, rec_neur, rec_syn, run_time = train_and_test_network(_tmp_params,
+                                                            model, data)
 
-    b_self_pred_aln = (b_i * b_o).sum(axis=1) / np.sqrt((b_i**2.).sum(axis=1) * (b_o**2.).sum(axis=1))
+        w_op = rec_syn["syn_hidden0_pyr_pop_to_output_output_pop_g"]
+        w_po = rec_syn["syn_output_output_pop_to_hidden0_pyr_pop_g"]
+        w_po_t = w_po.swapaxes(1, 2)
+        w_ip = rec_syn["syn_hidden0_pyr_pop_to_int_pop_g"]
+        w_pi = rec_syn["syn_hidden0_int_pop_to_pyr_pop_g"]
 
-    df_learn = pd.concat([df_learn,
-                          pd.DataFrame({
-                            "Epoch": np.repeat(epoch_ax,
-                                               params_base["n_runs"]),
-                            "Sim ID": np.tile(np.arange(params_base["n_runs"]),
-                                              params_base["n_test_run"]),
-                            "Accuracy": acc.flatten(),
-                            "Loss": loss.flatten(),
-                            "Model": model_name,
-                            "Method": method_name,
-                            "Spike Threshold": spike_th,
-                            "N Hidden": n_hidden
-                          })], ignore_index=True)
+        w_fb_aln = (w_op * w_po_t).sum(axis=(1,2)) / np.sqrt((w_op**2.).sum(axis=(1,2)) * (w_po_t**2.).sum(axis=(1,2)))
 
-    df_learn = pd.concat([df_learn,
-                          pd.DataFrame({
-                              "Epoch": np.repeat(AX_REC_MOD_PARAMS / T_SHOW_PATTERN,
-                                                 params_base["n_runs"]),
-                              "Sim ID": np.tile(np.arange(params_base["n_runs"]),
-                                                AX_REC_MOD_PARAMS.shape[0]),
-                              "Feedback Weight Alignment": w_fb_aln.flatten(),
-                              "Self-Prediction Forward Weight Alignment": w_self_pred_fwd_aln.flatten(),
-                              "Self-Prediction Backward Weight Alignment": w_self_pred_bw_aln.flatten(),
-                              "Self-Prediction Bias Alignment": b_self_pred_aln.flatten(),
-                              "Model": model_name,
-                              "Method": method_name,
-                              "Spike Threshold": spike_th,
-                              "N Hidden": n_hidden
-                          })], ignore_index=True)
+        w_self_pred_fwd_aln = (w_op * w_ip).sum(axis=(1,2)) / np.sqrt((w_op**2.).sum(axis=(1,2)) * (w_ip**2.).sum(axis=(1,2)))
 
-    df_runtime = pd.concat(
-       [df_runtime,
-        pd.DataFrame(
-            {"Runtime": run_time,
-             "Sim ID": np.arange(params_base["n_runs"]),
-             "Model": model_name,
-             "Method": method_name,
-             "Spike Threshold": spike_th,
-             "N Hidden": n_hidden}
-        )], ignore_index=True)
+        w_self_pred_bw_aln = (w_po * w_pi).sum(axis=(1,2)) / np.sqrt((w_po**2.).sum(axis=(1,2)) * (w_pi**2.).sum(axis=(1,2)))
+
+        b_i = rec_neur["neur_hidden0_int_pop_b"][:,0] # only use the first batch (they are all the same value across batches anyway)
+        b_o = rec_neur["neur_output_output_pop_b"][:,0] # ""
+
+        b_self_pred_aln = (b_i * b_o).sum(axis=1) / np.sqrt((b_i**2.).sum(axis=1) * (b_o**2.).sum(axis=1))
+
+        df_learn = pd.concat([df_learn,
+                            pd.DataFrame({
+                                "Epoch": np.repeat(epoch_ax,
+                                                params_base["n_runs"]),
+                                "Sim ID": np.tile(np.arange(params_base["n_runs"]),
+                                                params_base["n_test_run"]),
+                                "Accuracy": acc.flatten(),
+                                "Loss": loss.flatten(),
+                                "Model": model_name,
+                                "Method": method_name,
+                                "Spike Threshold": spike_th,
+                                "N Input": net_size_zip[0],
+                                "N Hidden": net_size_zip[1],
+                                "N Output": net_size_zip[2]
+                            })], ignore_index=True)
+
+        df_learn = pd.concat([df_learn,
+                              pd.DataFrame({
+                                "Epoch": np.repeat(AX_REC_MOD_PARAMS / T_SHOW_PATTERN,
+                                                    params_base["n_runs"]),
+                                "Sim ID": np.tile(np.arange(params_base["n_runs"]),
+                                                    AX_REC_MOD_PARAMS.shape[0]),
+                                "Feedback Weight Alignment": w_fb_aln.flatten(),
+                                "Self-Prediction Forward Weight Alignment": w_self_pred_fwd_aln.flatten(),
+                                "Self-Prediction Backward Weight Alignment": w_self_pred_bw_aln.flatten(),
+                                "Self-Prediction Bias Alignment": b_self_pred_aln.flatten(),
+                                "Model": model_name,
+                                "Method": method_name,
+                                "Spike Threshold": spike_th,
+                                "N Input": net_size_zip[0],
+                                "N Hidden": net_size_zip[1],
+                                "N Output": net_size_zip[2]
+                            })], ignore_index=True)
+
+        df_runtime = pd.concat(
+            [df_runtime,
+             pd.DataFrame(
+                {"Runtime": run_time,
+                 "Sim ID": np.arange(params_base["n_runs"]),
+                 "Model": model_name,
+                 "Method": method_name,
+                 "Spike Threshold": spike_th,
+                 "N Input": net_size_zip[0],
+                 "N Hidden": net_size_zip[1],
+                 "N Output": net_size_zip[2]}
+             )], ignore_index=True)
+
+        t1 = time.time()
+
+        t_est_left = ((t1 - t0) / (k_sweep + 1)) * (n_sweep - (k_sweep + 1))
+        file_log.write(time.strftime("%H:%M:%S", time.gmtime(t_est_left)) + "\n")
+        file_log.flush()
 
 df_learn.to_csv(os.path.join(BASE_FOLD, "df_learn.csv"), index=False)
 df_runtime.to_csv(os.path.join(BASE_FOLD, "df_runtime.csv"), index=False)
